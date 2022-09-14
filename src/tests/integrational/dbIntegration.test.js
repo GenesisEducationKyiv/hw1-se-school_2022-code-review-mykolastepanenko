e477859543f1b3434ff9b0c42a005b59c8b8b45f
Mykola Stepanenko <Nikolaua36@gmail.com>
2022-09-14 13:01:18 +0300

    moved subscription test from unit to integration and renamed to dbintegration test

--- src/tests/integrational/dbIntegration.test.js
+++ src/tests/integrational/dbIntegration.test.js
@@ -1,7 +1,7 @@
 import { writeEmail } from "../../services/database.js";
 import { createTestTxtFile, deleteTestTxtFile } from "./mocks.js";
 
-describe("Test subscribtion functions", () => {
+describe("Test of actions with db (txt file)", () => {
   const email = `test${(Math.random() * 100).toString().slice(0, 3)}@test.test`;
 
   beforeAll(createTestTxtFile);
